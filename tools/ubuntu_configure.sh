396fab9c651c76a532d75483c27cf71a3eff95f5
averagehuman <cyclebelfast@gmail.com>
2014-08-30 15:31:18 +0100

    prevent java license agreement dialogue

--- tools/ubuntu_configure.sh
+++ tools/ubuntu_configure.sh
@@ -123,6 +123,7 @@ chmod 644 /etc/apt/apt.conf.d/10periodic
 # oracle java ppa
 ###############################################################################
 add-apt-repository -y ppa:webupd8team/java
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | /usr/bin/debconf-set-selections
 
 ###############################################################################
 # docker ppa
